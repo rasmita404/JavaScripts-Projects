b44115a444e847d473ca3c3ca010fd86c6cf933b
rasmita404 <sahurasmita409@gmail.com>
2021-12-22 15:06:05 +0530

    Initial commit

---
+++ 1_JS_Drumkit/index.js
@@ -0,0 +1,16 @@
+function clicked(num){
+    const audio = this.document.querySelector(`audio[data-key="${num}"]`);
+    const key = this.document.querySelector(`.key[data-key="${num}"]`);
+    console.log(key);
+    if(!audio) return; //stops the function
+    
+    audio.currentTime =0;//rewind to the start
+    audio.play();
+    key.classList.add('playing');
+    const tout = setTimeout(removeTransition().call(key), 70);
+}
+function removeTransition()
+{
+    console.log(this);
+    this.classList.remove('playing');
+}
